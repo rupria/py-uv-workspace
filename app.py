041a4c3fe61af1a9bda7c70ca4b5e9288501b70a
rupria <rupria.jjb@gmail.com>
2026-07-16 16:41:01 +0900

    26.07.16_데이터최신화_1640

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
+
 import streamlit as st
+import pandas as pd
 
+header = ['학번','이름','전공']
+if "students" not in st.session_state:
+    st.session_state.students = [
+        ["202601", "홍길동", "컴퓨터공학"],
+        ["202602", "이순신", "데이터사이언스"],
+        ["202603", "유관순", "인공지능학"],
+    ]
 
-with st.form('form'):
-    chk1 = st.checkbox('낚시')
-    chk2 = st.checkbox('골프')
-    chk3 = st.checkbox('영화')
-    submit = st.form_submit_button('확인')
-    if submit:
-        if chk1:
-            st.write('낚시선택')
-        if chk2:
-            st.write('골프선택')
-        if chk3:
-            st.write("영화선택")
+st.title("통계")
+
+num = st.text_input('학번을 입력하세요.')
+name = st.text_input('이름을 입력하세요.')
+jungong = st.text_input('전공을 입력하세요.')
+
+if st.button("등록"):
+    if num and name and jungong:
+        st.session_state.students.append([num, name, jungong])
+        st.success(f"{name} 학생이 등록되었습니다.")
+
+df = pd.DataFrame(st.session_state.students, columns=header)
+st.table(df)
